9588110c978f395b8b665e0ed2057929a94a1737
Supriyahv9 <109582223+Supriyahv9@users.noreply.github.com>
2024-01-23 09:38:03 +0530

    Add files via upload

---
+++ MyntraTest.java
@@ -0,0 +1,19 @@
+package Basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MyntraTest {
+
+	@Test
+	public void LaunchTest() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.myntra.com/");
+		String actualtitle = driver.getTitle();
+		System.out.println(actualtitle);
+	}
+
+}
