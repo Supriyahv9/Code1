2b4d88b7909e0f37ae17bc0ab6775156fccc5503
shree887 <100765082+shree887@users.noreply.github.com>
2024-01-23 10:57:34 +0530

    added

--- src/test/java/Basic/AjioTest.java
+++ src/test/java/Basic/AjioTest.java
@@ -4,17 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class MyntraTest {
-
+public class AjioTest {
 	@Test
 	public void LaunchTest() {
 		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.myntra.com/");
+		driver.get("https://www.ajio.com/");
 		String actualtitle = driver.getTitle();
 		System.out.println(actualtitle);
-		System.out.println(actualtitle);
+		
 	}
-
+	
 }
